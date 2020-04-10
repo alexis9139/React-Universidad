a01bb6fab1f4a4583c977bb6c2a1d6b53064d138
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-10 14:19:08 -0300

    Expresiones en JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const saludo = <h1>Hola reactivo</h1>
+const nombre = 'alexis'
+const usuario = {
+    nombre: 'bruno',
+    edad: 27,
+    sexo: 'masculino'
+}
+function getUser(usuario) {
+    return `Hola me llamo ${usuario.nombre}`
+}
+// const saludo = <h1>Hola reactivo {nombre}</h1>
+// const saludo = <h1>hola llamo {usuario.nombre}, y tengo {usuario.edad + 1} años de edad.</h1>
+const saludo = <h1>{getUser(usuario)}</h1>
 const root = document.getElementById('root')
 
 ReactDOM.render(saludo, root)
