3cec130c2bc44880b117d85506d16b92e6591af4
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-11 14:40:08 -0300

    Usando métodos de clase para manejar Eventos

--- src/index.js
+++ src/index.js
@@ -7,19 +7,41 @@ class TarjetaFruta1 extends React.Component {
         this.state = {//this para hacer referencia a este componente
             cantidad: 0
         }
+        //tips para hacer los bind de una mejor forma
+        const METHODS = [
+            'agregar',
+            'quitar',
+            'limpiar'
+        ]
+        METHODS.forEach((method) => {
+            this[method] = this[method].bind(this)
+        })
+        // this.agregar = this.agregar.bind(this)
+        // this.quitar = this.quitar.bind(this)
+    }
+    agregar() {
+        this.setState({
+            cantidad: this.state.cantidad + 1
+        })
+    }
+    quitar() {
+        this.setState({
+            cantidad: this.state.cantidad - 1
+        })
+    }
+    limpiar() {
+        this.setState({
+            cantidad: 0
+        })
     }
     render() {
         return (
             <div>
                 <h1>{this.props.name}</h1>
                 <p>cantidad: {this.state.cantidad}</p>
-                <button onClick={() => {
-                    this.setState({
-                        cantidad: this.state.cantidad + 1
-                    })
-                }}
-                >
-                    Agregar</button>
+                <button onClick={this.agregar}>Agregar</button>
+                <button onClick={this.quitar}>Quitar</button>
+                <button onClick={this.limpiar}>Limpiar</button>
             </div>
         )
     }
