5c2acf74cd64064d3e9be01f61549df4aea51115
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-11 20:05:56 -0300

    Ejemplo de refactorizacion de componentes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TarjetaFruta from './componentes/TarjetaFruta'
+const App = () => (
+    <div>
+        <TarjetaFruta name="Kiwi"></TarjetaFruta>
+    </div>
+
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,60 +1,7 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-class TarjetaFruta1 extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {//this para hacer referencia a este componente
-            cantidad: 0
-        }
-        //tips para hacer los bind de una mejor forma
-        const METHODS = [
-            'agregar',
-            'quitar',
-            'limpiar'
-        ]
-        METHODS.forEach((method) => {
-            this[method] = this[method].bind(this)
-        })
-        // this.agregar = this.agregar.bind(this)
-        // this.quitar = this.quitar.bind(this)
-    }
-    agregar() {
-        this.setState({
-            cantidad: this.state.cantidad + 1
-        })
-    }
-    quitar() {
-        this.setState({
-            cantidad: this.state.cantidad - 1
-        })
-    }
-    limpiar() {
-        this.setState({
-            cantidad: 0
-        })
-    }
-    render() {
-        return (
-            <div>
-                <h1>{this.props.name}</h1>
-                <p>cantidad: {this.state.cantidad}</p>
-                <button onClick={this.agregar}>Agregar</button>
-                <button onClick={this.quitar}>Quitar</button>
-                <button onClick={this.limpiar}>Limpiar</button>
-            </div>
-        )
-    }
-}
-
-
-var comentario = "Este es un comentario"
-const App = () => (
-    <div>
-        <TarjetaFruta1 name="Kiwi"></TarjetaFruta1>
-    </div >
-
-)
+import App from './App'
 
 const root = document.getElementById('root')
 
